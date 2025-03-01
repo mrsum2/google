7b25c1cae97ad628f385467be34f17e7e0d8b5de
Clicker <unkownmalesir@gmail.com>
2025-03-01 11:30:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 <script>
+let ratings = {
+    minecraft: 0,
+    eaglercraft: 0,
+    basketbros: 0
+};
+
+function rateGame(game, rating) {
+    ratings[game] = rating;
+    document.getElementById(game + '-rating').innerText = `Rating: ${rating}`;
+}
+
 function goHome() {
     window.location.href = "index.html";
 }
